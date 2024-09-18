73a2d2f560dd013fef34f53f944c3dd93e79874d
Akiva Zonenfeld <akivazo1@gmail.com>
2024-09-18 13:06:52 +0300

    flights_status_checker

---
+++ flight_app/flights_success_checker.py
@@ -0,0 +1,86 @@
+import csv
+from datetime import datetime
+import sys, os
+
+class FlightSuccessChecker:
+    '''
+    For each flight update its status:
+        The flight will defined as 'success' if:
+            - the difference between the arrival and departure is greater or equal than 'min_minutes_delta' minutes.
+            - There is no more than 'max_success' successed flights arrivals before this flight arrival.
+        Otherwise the flight will defined as 'fail'.
+    '''
+    def __init__(self, flights_data, min_minutes_delta=180, max_success=20) -> None:
+        self.__flight_data = flights_data
+        self.__min_minutes_delta = min_minutes_delta
+        self.__max_success = max_success
+
+    def get_update_flights(self) -> list:
+        # sort the flight by arrival
+        sorted_flights = list(sorted(self.__flight_data, key=lambda flight_data: flight_data[1]))
+        self.__update_status_by_time_delta(sorted_flights)
+        self.__update_status_if_too_much_success(sorted_flights)
+        return sorted_flights
+    
+    
+    
+        
+    def __update_status_if_too_much_success(self, sorted_flights):
+        # count the successed flights 
+        success_count = 0
+        for row in sorted_flights:
+            if success_count >= self.__max_success:
+                # if we already have 20 or more successed flights, all other flights should be mark as fail 
+                row[3] = "fail"
+                continue
+            if row[3] == "success":
+                success_count += 1
+
+    def __get_minutes_delta(self, time1, time2):
+        # return the time delta in minutes between time1 and time2 
+        format = "%H:%M"
+        time1_dt = datetime.strptime(time1, format)
+        time2_dt = datetime.strptime(time2, format)
+
+        time_delta = time2_dt - time1_dt
+        return time_delta.total_seconds() / 60
+    
+    def __update_status_by_time_delta(self, sorted_flights):
+        for row in sorted_flights:
+            arrival = row[1]
+            departure = row[2]
+            
+            time_delta = self.__get_minutes_delta(time1=arrival, time2=departure)
+
+            if time_delta >= self.__min_minutes_delta:
+                # The time between arrival and departure is greater or equal than 180 minutes so mark as success
+                row[3] = "success"
+            else:
+                row[3] = "fail"
+        
+
+            
+def read_flights_data(csv_file):
+
+    csv_reader = csv.reader(csvfile=csv_file)
+    # skip the header line
+    header = next(csv_reader)
+    return list(csv_reader)
+
+def write_flights_data(csv_file, flights):
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        
+        # Write the data to the CSV file
+        writer.writerows(flights)
+
+if __name__ == "__main__":
+    
+    csv_file = sys.argv[1]
+
+    flights_data = read_flights_data(csv_file)
+
+    flights_status_checker = FlightSuccessChecker(flights_data=flights_data)
+    flights_data = flights_status_checker.get_update_flights()
+
+    write_flights_data(csv_file=csv_file, flights=flights_data)
---
+++ tests/test_flight_succes_checker.py
@@ -0,0 +1,125 @@
+import pytest
+from ..flight_app.flights_success_checker import FlightSuccessChecker
+
+def get_flights_status_checker(data):
+    return FlightSuccessChecker(flights_data=data)
+
+def test_no_flights():
+    flights_status_checker  = get_flights_status_checker(data=[])
+    res = flights_status_checker.get_update_flights()
+
+    assert res == []
+
+def test_all_success():
+    flights_status_checker = get_flights_status_checker(data=[
+        ["id3", "16:00", "20:31", ""],
+        ["id1", "00:00", "03:00", ""],
+        ["id2", "01:30", "05:00", ""],
+        
+    ])
+
+    res = flights_status_checker.get_update_flights()
+
+    # assert all success and sorted by arrival
+    assert res == [
+        ["id1", "00:00", "03:00", "success"],
+        ["id2", "01:30", "05:00", "success"],
+        ["id3", "16:00", "20:31", "success"]
+    ]
+
+
+def test_short_time_delta():
+    flights_status_checker = get_flights_status_checker(data=[
+        ["id3", "16:00", "16:01", ""],
+        ["id1", "00:00", "02:59", ""],
+        ["id2", "01:30", "05:00", ""],
+        ["id4", "15:30", "21:00", ""],
+        
+    ])
+
+    res = flights_status_checker.get_update_flights()
+
+    # assert correct status and sorted by arrival
+    assert res == [
+        ["id1", "00:00", "02:59", "fail"],
+        ["id2", "01:30", "05:00", "success"],
+        ["id4", "15:30", "21:00", "success"],
+        ["id3", "16:00", "16:01", "fail"]
+    ]
+
+def test_too_much_success():
+    flights_status_checker = get_flights_status_checker(data=[
+        ["id1", "00:00", "03:01", ""],
+        ["id2", "01:00", "04:01", ""],
+        ["id3", "02:00", "05:01", ""],
+        ["id4", "03:00", "03:01", ""],
+        ["id5", "04:00", "07:01", ""],
+        ["id6", "05:00", "08:01", ""],
+        ["id7", "06:00", "09:01", ""],
+        ["id8", "07:00", "10:01", ""],
+        ["id9", "08:00", "11:01", ""],
+        ["id10", "09:00", "12:01", ""],
+        ["id11", "10:00", "13:01", ""],
+        ["id12", "11:00", "14:01", ""],
+        ["id13", "12:00", "15:01", ""],
+        ["id14", "13:00", "16:01", ""],
+        ["id15", "14:00", "17:01", ""],
+        ["id16", "15:00", "18:01", ""],
+        ["id17", "16:00", "19:01", ""],
+        ["id18", "17:00", "20:01", ""],
+        ["id19", "18:00", "21:01", ""],
+        ["id20", "19:00", "22:01", ""],
+        ["id21", "20:00", "23:01", ""],
+        ["id22", "20:05", "23:10", ""],
+    ])
+
+    res = flights_status_checker.get_update_flights()
+
+    assert res ==   [
+        ["id1", "00:00", "03:01", "success"],
+        ["id2", "01:00", "04:01", "success"],
+        ["id3", "02:00", "05:01", "success"],
+        ["id4", "03:00", "03:01", "fail"],
+        ["id5", "04:00", "07:01", "success"],
+        ["id6", "05:00", "08:01", "success"],
+        ["id7", "06:00", "09:01", "success"],
+        ["id8", "07:00", "10:01", "success"],
+        ["id9", "08:00", "11:01", "success"],
+        ["id10", "09:00", "12:01", "success"],
+        ["id11", "10:00", "13:01", "success"],
+        ["id12", "11:00", "14:01", "success"],
+        ["id13", "12:00", "15:01", "success"],
+        ["id14", "13:00", "16:01", "success"],
+        ["id15", "14:00", "17:01", "success"],
+        ["id16", "15:00", "18:01", "success"],
+        ["id17", "16:00", "19:01", "success"],
+        ["id18", "17:00", "20:01", "success"],
+        ["id19", "18:00", "21:01", "success"],
+        ["id20", "19:00", "22:01", "success"],
+        ["id21", "20:00", "23:01", "success"],
+        ["id22", "20:05", "23:10", "fail"],
+
+    ]  
+
+def test_with_custom_params():
+    # min_delay: 5 minuts
+    # max_success: 3
+    flights_status_checker = FlightSuccessChecker(flights_data=[
+        ["id1", "00:00", "00:05", ""],
+        ["id3", "13:00", "13:04", ""], # not enough delay
+        ["id2", "02:30", "16:05", ""],
+        ["id4", "21:00", "23:00", ""],
+        ["id5", "21:01", "23:00", ""], # too many success
+        ], min_minutes_delta=5, max_success=3)
+
+    res = flights_status_checker.get_update_flights()
+
+    assert res == [
+        ["id1", "00:00", "00:05", "success"],
+        ["id2", "02:30", "16:05", "success"],
+        ["id3", "13:00", "13:04", "fail"], # not enough delay
+        ["id4", "21:00", "23:00", "success"],
+        ["id5", "21:01", "23:00", "fail"], # too many success
+
+    ]
+    
